2d12fe1520f8d94e98f615431ed40be5972fdde8
Kusum Ghimire <kghimire@himalayantrchies.com>
2020-12-30 16:58:52 +0545

    script.js created

---
+++ script.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelector('.buttons');
+const screen = document.querySelector('.screen');
+const audio = document.querySelector('.audio');
+
+buttons.addedEventListner('click', (e) => {
+
+});
